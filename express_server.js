c34670217625433c0fc5465384703b789c03b4bc
jadenlee0519 <jadenlee0519@gmail.com>
2022-07-13 14:11:30 -0700

    learning

--- express_server.js
+++ express_server.js
@@ -1,90 +1,154 @@
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
-app.use(express.urlencoded({ extended: true }));
-const cookieParser = require('cookie-parser')
-app.use(cookieParser())
-// EJS template
-app.set("view engine", "ejs");
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-function generateRandomString() {
-  let randomStr = Math.random().toString(36).substring(6);
-  if (randomStr.length !== 6) {
-    randomStr = generateRandomString();
-  }
-  return randomStr;
-}
+// DataBase
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-// ROUTING
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  if (longURL) {
-  res.redirect(longURL);
+
+const usersDb = {
+  admin: {
+    id: "admin",
+    email: "admin@admin.io",
+    password: "admin",
+  },
+
+};
+
+// Helper Funcs
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2, 8);
+}
+
+const addLinkToDatabase = (id, url) => {
+  urlDatabase[id] = url;
+}
+
+const getUserByCookie = (cookie) => {
+  for (let user in usersDb) {
+    if (usersDb[user].id === cookie) {
+      return usersDb[user];
+    }
+  }
+  return undefined;
+}
+
+const getUserByEmail = (email) => {
+  for (let user in usersDb) {
+    if (usersDb[user].email === email) {
+      return usersDb[user];
+    }
+  }
+  return undefined;
+}
+
+const passwordCheck = (password) => {
+  for (let user in usersDb) {
+    if (usersDb[user].password === password) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Settings
+const express = require("express");
+const app = express();
+const PORT = 8080;
+app.set("view engine", "ejs");
+const cookieParser = require('cookie-parser');
+const e = require("express");
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Post
+
+app.post("/register", (req, res) => {
+  const user_id = generateRandomString();
+  if (!req.body.email.trim() || !req.body.password.trim()) {
+    res.status(400).send("Please enter an email and password");
+  } else if (getUserByEmail(req.body.email)) {
+    res.status(400).send("Account already exists");
   } else {
-    res.status(404);
+    res.cookie("user_id", user_id);
+    usersDb[user_id] = { "id": user_id, "email": req.body.email, "password": req.body.password };
+    res.redirect("/urls");
   }
 });
-// Allows us to render a new website URL and displays it with the urls_new template
-app.get("/urls/new", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"]
+
+app.post("/login", (req, res) => {
+  if (!getUserByEmail(req.body.email.trim())) {
+    res.status(403).send("User does not exist")
+  } else if (!passwordCheck(req.body.password))
+    res.status(403).send("Password is incorrect")
+  else {
+    res.cookie("user_id", getUserByEmail(req.body.email).id);
+    res.redirect("/urls");
   }
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id")
+  res.redirect("/urls");
+});
+
+app.post("/urls", (req, res) => {
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  addLinkToDatabase(id, longURL);
+  res.redirect(`/urls/${id}`);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]
+  res.redirect("/");
+});
+
+app.post("/urls/:id/update", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/");
+});
+
+// Get
+
+app.get("/", (req, res) => {
+  const templateVars = { urls: urlDatabase, user: getUserByCookie(req.cookies["user_id"]) };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase, user: getUserByCookie(req.cookies["user_id"]) };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  const templateVars = { user: getUserByCookie(req.cookies["user_id"]) };
   res.render("urls_new", templateVars);
 });
-// delete url
-app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id]
-  res.redirect('/urls')
-})
-// edit request
-app.post('/urls/:id/', (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL
-  res.redirect('/urls')
-})
-// login functionality
-app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username)
-  res.redirect('/urls')
-})
-// Generates short URL that is added to database and redirected to urls/:id
-app.post("/urls", (req, res) => {
-  let newId = generateRandomString();
-  urlDatabase[newId] = req.body.longURL;
-  res.redirect(`urls/${newId}`); 
-});
 
-// logout endpoint
-  app.post('/logout', (req, res) => {
-    res.clearCookie('username')
-    res.redirect('/urls')
-  })
-
-// Displays short URL and long URL
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { 
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id], 
-    username: req.cookies["username"] 
-  };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: getUserByCookie(req.cookies["user_id"]) };
   res.render("urls_show", templateVars);
 });
 
-// Displays our urls in the urlDatabase by using urls_index template
-app.get("/urls", (req, res) => {
-  let templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase
-  };
-  res.render("urls_index", templateVars);
+app.get("/register", (req, res) => {
+  const templateVars = { user: getUserByCookie(req.cookies["user_id"]) };
+  res.render("urls_register", templateVars);
 });
-app.get("/", (req, res) => {
-  res.send("Hello!");
+
+app.get("/login", (req, res) => {
+  const templateVars = { user: getUserByCookie(req.cookies["user_id"]) };
+  res.render("urls_login", templateVars)
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+// Port listener
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
