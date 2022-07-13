ce8741ead757d2a9e43ad32419c11c1ea681602b
jadenlee0519 <jadenlee0519@gmail.com>
2022-07-12 20:43:13 -0700

    creat login

--- express_server.js
+++ express_server.js
@@ -1,77 +1,90 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
-const generateRandomString = () => {
-  let tinyUrl = "";
-  const characterList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 6; i++) {
-    tinyUrl += characterList.charAt(Math.floor(Math.random() * characterList.length));
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+// EJS template
+app.set("view engine", "ejs");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+function generateRandomString() {
+  let randomStr = Math.random().toString(36).substring(6);
+  if (randomStr.length !== 6) {
+    randomStr = generateRandomString();
   }
-  return tinyUrl;
-};
-
+  return randomStr;
+}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-});
-
-// URLs
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.post('/urls', (req, res) => {
-  const tinyUrl = generateRandomString();
-  urlDatabase[tinyUrl] = req.body.longURL;
-  res.redirect('urls/' + tinyUrl);
-});
-
-// Add URL
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-//
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
-
-// Delete URL
-app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect('/urls');
-});
-
-// Edit URL
-app.post('/urls/:id/edit', (req, res) => {
-  urlDatabase[req.params.id] = req.body.newURL;
-  res.redirect('/urls');
-});
-
-// URL json
+// ROUTING
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  if (longURL) {
+  res.redirect(longURL);
+  } else {
+    res.status(404);
+  }
+});
+// Allows us to render a new website URL and displays it with the urls_new template
+app.get("/urls/new", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
+});
+// delete url
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id]
+  res.redirect('/urls')
+})
+// edit request
+app.post('/urls/:id/', (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL
+  res.redirect('/urls')
+})
+// login functionality
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username)
+  res.redirect('/urls')
+})
+// Generates short URL that is added to database and redirected to urls/:id
+app.post("/urls", (req, res) => {
+  let newId = generateRandomString();
+  urlDatabase[newId] = req.body.longURL;
+  res.redirect(`urls/${newId}`); 
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+// logout endpoint
+  app.post('/logout', (req, res) => {
+    res.clearCookie('username')
+    res.redirect('/urls')
+  })
+
+// Displays short URL and long URL
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id], 
+    username: req.cookies["username"] 
+  };
+  res.render("urls_show", templateVars);
+});
+
+// Displays our urls in the urlDatabase by using urls_index template
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
+});
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
