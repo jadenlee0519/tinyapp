cf52eccb7c7a3eb6148bfc89f080b16337d5a600
jadenlee0519 <jadenlee0519@gmail.com>
2022-07-12 18:14:35 -0700

    add edit button

--- express_server.js
+++ express_server.js
@@ -1,38 +1,26 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const generateRandomString = () => {
+  let tinyUrl = "";
+  const characterList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    tinyUrl += characterList.charAt(Math.floor(Math.random() * characterList.length));
+  }
+  return tinyUrl;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(express.urlencoded({ extended: true }));
-
-app.set("view engine", "ejs");
-
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-function generateRandomString() {
-  let text = "";
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < 6; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
-};
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.get("/u/:id", (req, res) => {
@@ -40,17 +28,49 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
+// URLs
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
-})
+app.post('/urls', (req, res) => {
+  const tinyUrl = generateRandomString();
+  urlDatabase[tinyUrl] = req.body.longURL;
+  res.redirect('urls/' + tinyUrl);
+});
 
+// Add URL
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+// Delete URL
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
+// Edit URL
+app.post('/urls/:id/edit', (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  res.redirect('/urls');
+});
+
+// URL json
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
